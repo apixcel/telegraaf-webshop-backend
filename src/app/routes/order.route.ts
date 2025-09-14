7ba37ddf132ccc381e0f325d9d71a6cd6f6f02f7
Md Rifajul Islam (Rifaj) <devrifaj@gmail.com>
2025-09-14 12:08:31 +0600

    feat: update

--- src/app/routes/order.route.ts
+++ src/app/routes/order.route.ts
@@ -3,8 +3,9 @@ import authMiddleware from "../middlewares/authValidation";
 import orderController from "../controller/order.controller";
 import { upload } from "../middlewares/multer";
 const router = Router();
-// router.use(authMiddleware.isAuthenticatedUser());
+router.use(authMiddleware.isAuthenticatedUser());
 router.post("/create-order", upload.single("file"), orderController.createOrder);
+router.get("/get-orders", orderController.getOrders);
 
 const orderRoute = router;
 export default orderRoute;
