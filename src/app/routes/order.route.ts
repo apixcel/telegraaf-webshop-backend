83216f6d8ed9bcd55769ff74c9a477919b81fd4d
Nazmul Islam Sakib <sakibsarkar>
2025-09-16 21:19:46 +0600

    feat: Update

--- src/app/routes/order.route.ts
+++ src/app/routes/order.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import orderController from "../controller/order.controller";
+import authMiddleware from "../middlewares/authValidation";
 import { upload } from "../middlewares/multer";
 const router = Router();
 
-// router.use(authMiddleware.isAuthenticatedUser());
+router.use(authMiddleware.isAuthenticatedUser());
 // @ts-expect-error: Unreachable code error
 router.post("/create-order", upload.single("file"), orderController.createOrder);
 router.get("/get-orders", orderController.getOrders);
